07585c90da461dedbdb51c1720aae1bab136d1cb
anus26 <anusraza268gmail.com>
2025-02-10 21:41:15 -0800

    modified

--- index.js
+++ index.js
@@ -12,15 +12,11 @@ import postRoutes from "./src/routes/post.routes.js"
 import likeRoutes from "./src/routes/like.routes.js"
 import sharerouter from './src/routes/share.routes.js'
 
-const corsOptions = {
-  origin: 'http://localhost:5173',
-  credentials: true,
-};
    
+
 const app = express();
-app.use(cors(corsOptions));
+app.use(cors())
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 const JWT_TOKEN_SECRET=process.env.JWT_TOKEN_SECRET
 
