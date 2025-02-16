019264eafce29faffab912b6dd6f83c270b4ee84
anus26 <anusraza268gmail.com>
2025-02-15 19:25:33 -0800

    fixed error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express"
-import cors  from "cors"
+// import cors  from "cors"
 import connectDB from "./src/db/index.js"
 import cookieParser  from 'cookie-parser'
 import jwt from "jsonwebtoken"
@@ -13,15 +13,21 @@ import likeRoutes from "./src/routes/like.routes.js"
 import sharerouter from './src/routes/share.routes.js'
 
    
-const app = express();
-app.use(cors({
-  origin: ["https://comment-react.vercel.app"],  
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}));
-// Handle preflight requests properly
-app.options("*", cors()); 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://comment-react.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  
+  // Agar preflight request hai to OK return karein
+  if (req.method === "OPTIONS") {
+      return res.status(200).send("OK");
+  }
+  
+  next();
+});
+
+
 
 app.use(express.json());
 app.use(cookieParser())
